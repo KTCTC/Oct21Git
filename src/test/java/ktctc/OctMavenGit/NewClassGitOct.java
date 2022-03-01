3968e71beb75e8c1696dd72d324f58dc8d4d0b7f
KTCTC <ktcorporatetrainingcentre@gmail.com>
2022-03-01 20:44:27 +0530

    updated code 13

---
+++ src/test/java/ktctc/OctMavenGit/NewClassGitOct.java
@@ -0,0 +1,11 @@
+package ktctc.OctMavenGit;
+
+public class NewClassGitOct {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+	}
+
+}
