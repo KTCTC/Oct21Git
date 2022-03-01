72b3509e5981d2237eba318cdcafac2002a9e5f0
KTCTC <ktcorporatetrainingcentre@gmail.com>
2022-03-01 21:04:17 +0530

    Updated code by ganesh 15

--- src/test/java/ktctc/OctMavenGit/NewClassGitOct.java
+++ src/test/java/ktctc/OctMavenGit/NewClassGitOct.java
@@ -6,6 +6,7 @@ public class NewClassGitOct {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
 		System.out.println("Hello 2");
+		System.out.println("Updated by Ganesh 1");
 
 	}
 
---
+++ src/test/java/ktctc/OctMavenGit/SampleGit2.java
@@ -0,0 +1,11 @@
+package ktctc.OctMavenGit;
+
+public class SampleGit2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi welcome to KTCTC");
+
+	}
+
+}
